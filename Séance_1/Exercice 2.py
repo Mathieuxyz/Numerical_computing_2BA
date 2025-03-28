6179b3696054081d2b22a1e3784a4e12ca73438e
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-03-28 11:28:26 +0100

    initial commit

--- Séance_1/Exercice 2.py
+++ Séance_1/Exercice 2.py
@@ -1,3 +1,20 @@
 import numpy as np
 
-print(np.conjugate(1-2j))
+exo1 = np.array([38, 24, 32, 45, 40])
+exo2 = np.array([23, 25, 31, 29, 15])
+exo3 = np.array([26, 34, 30, 28, 37])
+
+matrix = np.array([exo1,exo2,exo3])
+
+def student_total_time(matrix):
+    for i in range(len(matrix[0, :])):
+        time = 0
+        for n in range(len(matrix[:,0])):
+            time += matrix[n,i]
+        return (i+1, n)
+
+print(student_total_time(matrix))
+
+#total time 1h45 minutes
+def remaining_time(matrix, total_time):
+    time = total_time*(60**2)
