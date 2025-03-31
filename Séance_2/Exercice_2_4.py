863fec1279f55d68afb2def274dd4864460c5a1c
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-03-31 11:49:51 +0200

    Fin de la séance, ajuster les températures par mois et seulement ensuite finir la régression linéaire

--- Séance_2/Exercice_2_4.py
+++ Séance_2/Exercice_2_4.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def temperatures():
+def temperatures(): #working
     
     years = 10
     
@@ -14,4 +15,35 @@ def temperatures():
     
     return Temperatures
 
-print(temperatures())
+def average_day_temperature(): #working
+
+    matrix = temperatures()
+
+    average_temperatures_array = np.zeros(shape = (1, 365))
+    
+    for i in range(len(matrix[0, :])):
+    
+        summ = 0
+    
+        for n in range(len(matrix[:, 0])):
+    
+            summ += matrix[n, i]
+
+        average = summ/len(matrix[:, 0])
+
+        average_temperatures_array[0, i] = average
+    
+    return average_temperatures_array
+
+def cloud():
+
+    y = average_day_temperature()
+    x = []
+
+    for i in range(len(y[0, :])):
+        x.append(i+1)
+
+    plt.scatter(x,y)
+    plt.show()
+
+cloud()
