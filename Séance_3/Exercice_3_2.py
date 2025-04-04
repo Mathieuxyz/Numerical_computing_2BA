252f94be5d3dd228663e438cec9ffc932bf94464
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-04-04 11:57:37 +0200

    Tout fonctione pour Exercice_3_2 mais la bouboule ne reconnaît pas où est le mur et donc explose pas en conséquent dessus

--- Séance_3/Exercice_3_2.py
+++ Séance_3/Exercice_3_2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 Vini = float(input('Initial speed (m/s) ? : '))
 alpha = float(input('Angle of shooting (°) ? : '))
+alpha = np.radians(alpha)
 dt = 0.13 #s/tic
 
 g = 9.81
@@ -17,19 +18,29 @@ def initial_conditions(Vini: float, alpha: float): #working with (x, y, z=0)
 def position():
 
     A0, B0, P0 = initial_conditions(Vini, alpha)
-    
+
     t = 0
     
+
     while True:
         t += dt
         P = (P0) + (B0 * t) + (0.5 * A0 * (t**2))
-        if P[0] > 100 or P[1] < 0 :
+        if 99 <= P[1] <= 101 and 0 <= P[1] <= 200 :
+            plt.plot(P[0], P[1], marker = '*', color = 'red', markersize = 15)
+            break
+        elif P[1] < 0 :
+            plt.plot(P[0], P[1], marker = '*', color = 'red', markersize = 15)
             break
         else:
-            px.append(P[0])
-            py.append(P[1])
             plt.plot(P[0], P[1], marker = 'o', color = 'green')
-            plt.pause(0.1)
+            plt.pause(0.05) #indispensable si on souhaite que les points s'affichent harmonieusement l'un à la suite de l'autre
+    
+    plt.xlim(-10,130)
+    plt.ylim(-10, 300)
+    plt.xlabel('Position x')
+    plt.ylabel('Postion y')
+    plt.title('Simulation tir de bouboule')
+    plt.legend()
     plt.show()
 
 position()
