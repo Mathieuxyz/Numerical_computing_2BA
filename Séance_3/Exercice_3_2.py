a894ce0814171798f70ad29fd4edf6fe78880cce
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-04-04 10:49:29 +0200

    initialisation Exercice_3_2

---
+++ Séance_3/Exercice_3_2.py
@@ -0,0 +1,9 @@
+
+Vini = float(input('Initial speed (m/s) ? : '))
+alpha = float(input('Angle of shooting (°) ? : '))
+
+print(Vini)
+print(alpha)
+
+def initial_conditions(Vini: float, alpha: float):
+    return None
